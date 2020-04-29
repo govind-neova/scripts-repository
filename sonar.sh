b150540023e5fc84367d41235b9e3091adbee110
root <root@ip-172-31-22-143.ec2.internal>
2020-04-29 05:35:47 +0000

    Updated sonar.sh file

--- sonar.sh
+++ sonar.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+project_name="lacework-repo1" 
 echo $SONAR_SCANNER_PATH
-
-echo "$1"
-echo "$2"
-
 #creating a folder
 file=sonar_task
-if  [ -d $file ] 
+if  [ -d $file ]
 then
-         echo "$file exist already...removing the directory"     
+         echo "$file exist already...removing the directory"
          rm -rf $file
 else
          echo "$file does not exist"
@@ -17,26 +14,23 @@ fi
 
 mkdir -p $file
 
-#cloning the repository
-echo "Cloning the git repository from URL $1"
 cd $file
-git clone "$1"
 
-#function to check currently working branch
+#################
 
-cd $2
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$BRANCH" == "master" ]]; then
-  echo "You are on master branch"
-  #exit 1;
+if [ -z "$2" ]
+then
+	git clone "$1"
+else
+	git clone -b $2 $1
 fi
 
-##creating configuration files in directory
+cd `ls -d */`
 
 touch sonar-project.properties
 
-i1=sonar.projectKey=puppeteer
-i2=sonar.projectKey=puppeteer
+i1=sonar.projectKey=$project_name
+i2=sonar.projectName=$project_name
 i3=sonar.projectVersion=1.0
 i4=sonar.sources=.
 
@@ -51,5 +45,6 @@ echo "$i3" >> sonar-project.properties
 echo "$i4" >> sonar-project.properties
 
 #run sonar-scanner
+echo $SONAR_SCANNER_PATH
+bash $SONAR_SCANNER_PATH
 #/opt/sonar-scanner/bin/sonar-scanner
-$SONAR_SCANNER_PATH
